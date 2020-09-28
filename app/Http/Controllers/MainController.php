3aaa38eef4b7058755db7c38636773c763afc41e
eugene330 <eermol20@gmail.com>
2020-09-28 14:36:28 +0300

    add eloquent many-to-many add BasketController.php add basket.blade.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -31,13 +31,4 @@ class MainController extends Controller
         return view('product', ['product' => $product]);
     }
 
-    public function basket()
-    {
-        return view('basket');
-    }
-
-    public function basketPlace()
-    {
-        return view('order');
-    }
 }
