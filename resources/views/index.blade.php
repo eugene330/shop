9542d501a3b6cf72413a03dfa97b0fec3d424738
eugene330 <eermol20@gmail.com>
2020-09-28 13:15:13 +0300

    add eloquent categories and products

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,9 @@
     <div class="starter-template">
         <h1>Все товары</h1>
         <div class="row">
-            @include('card')
+            @foreach($products as $product)
+                @include('card', compact('product'))
+            @endforeach
         </div>
     </div>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,8 +9,8 @@
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/starter-template.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/starter-template.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
