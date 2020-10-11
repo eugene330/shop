9caf348884195bb7a94248c35c95c43264573f23
eugene330 <eermol20@gmail.com>
2020-10-11 16:43:30 +0300

    Pagination QueryBuilder Filters

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,14 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Http\Requests\ProductsFilterRequest;
 use App\Product;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
+    public function index(ProductsFilterRequest $request)
     {
-        $products = Product::get();
+        $productQuery = Product::query();
+
+        if ($request->filled('price_from')) {
+            $productQuery->where('price', '>=', $request->price_from);
+        }
+
+        if ($request->filled('price_to')) {
+            $productQuery->where('price', '<=', $request->price_to);
+        }
+
+        foreach (['hit', 'new', 'recommend'] as $field) {
+            if ($request->has($field)) {
+                $productQuery->where($field, 1);
+            }
+        }
+
+        $products = $productQuery->paginate(6)->withPath("?" . $request->getQueryString());
+
         return view('index', compact('products'));
     }
 
--- resources/views/auth/categories/index.blade.php
+++ resources/views/auth/categories/index.blade.php
@@ -42,6 +42,7 @@
             @endforeach
             </tbody>
         </table>
+        {{ $categories->links() }}
         <a class="btn btn-success" type="button"
            href="{{ route('categories.create') }}">Добавить категорию</a>
     </div>
