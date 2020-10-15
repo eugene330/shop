5e0900cad24e1cc3e860f3265b41c04a52d440bb
eugene330 <eermol20@gmail.com>
2020-10-15 13:46:27 +0300

    Model injection new Class Basket.php

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -3,31 +3,25 @@
 namespace App\Http\Controllers;
 
 
+use App\Classes\Basket;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class BasketController extends Controller
 {
     public function basket()
     {
-        $orderId = session('orderId');
-        $order = Order::findOrFail($orderId);
-
+        $order = (new Basket())->getOrder();
         return view('basket', compact('order'));
     }
 
     public function basketConfirm(Request $request)
     {
-        $orderId = session('orderId');
-        $order = Order::findOrFail($orderId);
-        $success = $order->saveOrder($request->name, $request->phone);
-
-        if ($success) {
+        if ((new Basket())->saveOrder($request->name, $request->phone)) {
             session()->flash('success', 'Ваш заказ принят в обработку');
         } else {
-            session()->flash('warning', 'Случилась ошибка');
+            session()->flash('warning', 'Добавлен не доступен для заказа в полном объеме');
         }
 
         Order::eraseOrderSum();
@@ -37,58 +31,32 @@ class BasketController extends Controller
 
     public function basketPlace()
     {
-        $orderId = session('orderId');
-        $order = Order::findOrFail($orderId);
+        $basket = new Basket();
+        $order = $basket->getOrder();
+
+        if(!$basket->countAvailable()){
+            session()->flash('warning', 'Добавлен не доступен для заказа в полном объеме');
+            return redirect()->route('basket');
+        }
         return view('order', compact('order'));
     }
 
     public function basketAdd(Product $product)
     {
-        $orderId = session('orderId');
+        $result = (new Basket(true))->addProduct($product);
 
-        if (is_null($orderId)) {
-            $order = Order::create();
-            session(['orderId' => $order->id]);
-        } else {
-            $order = Order::findOrFail($orderId);
+        if($result){
+            session()->flash('success', 'Добавлен товар ' . $product->name);
+        }else{
+            session()->flash('warning', 'Добавлен ' . $product->name . ' в большем кол-ве не доступен для заказа');
         }
-
-        if ($order->products->contains($product->id)) {
-            $pivotRow = $order->products()->where('product_id', $product->id)->first()->pivot;
-            $pivotRow->count++;
-            $pivotRow->update();
-        } else {
-            $order->products()->attach($product->id);
-        }
-
-        if (Auth::check()) {
-            $order->user_id = Auth::id();
-            $order->save();
-        }
-
-        Order::changeFullSum($product->price);
-
-        session()->flash('success', 'Добавлен товар ' . $product->name);
 
         return redirect()->route('basket');
     }
 
     public function basketRemove(Product $product)
     {
-        $orderId = session('orderId');
-        $order = Order::findOrFail($orderId);
-
-        if ($order->products->contains($product->id)) {
-            $pivotRow = $order->products()->where('product_id', $product->id)->first()->pivot;
-            if ($pivotRow->count < 2) {
-                $order->products()->detach($product->id);
-            } else {
-                $pivotRow->count--;
-                $pivotRow->update();
-            }
-        }
-
-        Order::changeFullSum(-$product->price);
+        (new Basket())->removeProduct($product);
 
         session()->flash('warning', 'Удален товар ' . $product->name);
 
