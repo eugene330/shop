111581b9f6b59ad804fff83eaf3a9c13cf877a2b
eugene330 <eermol20@gmail.com>
2020-10-15 11:51:38 +0300

    Count for products Soft delete Bugs fixed

--- resources/views/auth/products/form.blade.php
+++ resources/views/auth/products/form.blade.php
@@ -88,6 +88,15 @@
                     </div>
                 </div>
                 <br>
+                <div class="input-group row">
+                    <label for="count" class="col-sm-2 col-form-label">Количество: </label>
+                    <div class="col-sm-2">
+                        @include('auth.layouts.error', ['fieldName' => 'count'])
+                        <input type="text" class="form-control" name="count" id="count"
+                               value="@isset($product){{ $product->count }}@endisset">
+                    </div>
+                </div>
+                <br>
                 @foreach([
     'hit'=>'Хит',
     'new'=>'Новинка',
@@ -97,8 +106,8 @@
                         <label for="name" class="col-sm-2 col-form-label">{{ $title }}: </label>
                         <div class="col-sm-6">
                             <input type="checkbox" class="form-control" name="{{ $field }}" id="{{ $field }}"
-                            @if(isset($product) && $product->$field === 1)
-                                checked="checked"
+                                   @if(isset($product) && $product->$field === 1)
+                                   checked="checked"
                                 @endif
                             >
                         </div>
